7303e647a6e29fcc18f1403ed4e749ec1bd24abc
omar :) <70287205+odlo@users.noreply.github.com>
2020-10-15 16:47:37 -0400

    final

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
     var panel_open = false;
     $(".drop-button1").click(function(){
-        if (panel_open == true) {
+        if (panel_open == false) {
             $(".body-text1").slideDown("slow");
-            panel_open = false;
+            panel_open = true;
         } else {
             $(".body-text1").slideUp("slow");
             panel_open = false;
         }
     })
+})
+
+$('img').on('click', function(e) {
+    $('#imgViewer').html('').append( $(e.currentTarget).clone().removeClass('img-responsive').removeClass('img-thumbnail') )
+    $('#viewImg').modal('show')
+})
+
+$('img').each(function(i,e) {
+    $(e).wrap('<div class="img-wrapper"></div>')
 })
